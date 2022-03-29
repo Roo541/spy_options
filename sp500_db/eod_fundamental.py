089a2e88692345c63e13028bac71fb065dc4f4d6
Roo541 <lahiru.fernando54@gmail.com>
2022-03-29 16:46:54 -0700

    sp500 options project

---
+++ sp500_db/eod_fundamental.py
@@ -0,0 +1,16 @@
+import requests
+import keys.eod_key as eod_key
+
+def fundamental_data(symbol):
+    API_URL = r'https://eodhistoricaldata.com/api/fundamentals/%s.US?' %symbol
+
+    data_1 = {
+                'api_token': eod_key.eod_key,         
+            }
+
+    session = requests.Session()
+    r = session.get(API_URL, params = data_1)
+    data = r.json()
+    
+    return data
+print(eod_key.eod_key)
